6b9d06ea3e1c3e04937d3b0f3b752e95c9bc9195
nobuti <buti@nobuti.com>
2017-03-12 17:55:45 +0100

    Map modals.

---
+++ _javascripts/components/modals.js
@@ -0,0 +1,30 @@
+var $ = require('jquery');
+
+var getTargetHTML = function (elem) {
+  var id = elem.attr('data-modal-id');
+  var target = $('[data-modal="' + id + '"]');
+  return target.length > 0 && target;
+};
+
+module.exports = {
+  init: function () {
+    var triggers = $('[data-modal-id]');
+
+    triggers.each(function (index, trigger) {
+      var el = $(trigger);
+      var target = getTargetHTML(el);
+
+      if (target) {
+        target.on('click', '[data-modal-close]', function (e) {
+          e.preventDefault();
+          target.fadeOut(200);
+        });
+
+        el.on('click', function (e) {
+          e.preventDefault();
+          target.fadeIn(200);
+        });
+      }
+    });
+  }
+};
